27c88560ff005004a1a5c57b18dd94f155038630
mariothedog <mariothedogmc@gmail.com>
2020-06-02 15:43:30 +0100

    Adding a /joingame and /leavegame command along with automatic kicking upon leaving the server.

---
+++ Util.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace TEHub
+{
+    class Util
+    {
+        public static string CapitalizeEachWord(string str)
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+            return textInfo.ToTitleCase(str);
+        }
+    }
+}
